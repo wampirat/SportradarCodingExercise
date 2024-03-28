eb3b8ac1136e7c77dabc1295d1cedcb8ce5b7803
wampir <mateuszsawicki89@gmail.com>
2024-03-28 15:55:21 +0100

    Align package name

--- src/main/java/com/sportradar/codingexercise/Main.java
+++ src/main/java/com/sportradar/codingexercise/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.sportradar.codingexercise;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
