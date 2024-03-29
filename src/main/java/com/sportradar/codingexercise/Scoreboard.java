fcc324e8c1bb4adb0b163716d7f82c93288aa981
wampirat <mateuszsawicki89@gmail.com>
2024-03-29 18:03:34 +0100

    Scoreboard shall be able to tell if two teams are in the same match
    Introduced Scoreboard::matchInProgress(String homeTeamName, String awayTeamName)

--- src/main/java/com/sportradar/codingexercise/Scoreboard.java
+++ src/main/java/com/sportradar/codingexercise/Scoreboard.java
@@ -1,10 +1,24 @@
 package com.sportradar.codingexercise;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Scoreboard {
+    Map<String, Match> matches = new HashMap<>();
+
+    private String calculateMatchKey(String homeTeamName, String awayTeamName)
+    {
+        return STR."\{homeTeamName}_\{awayTeamName}";
+    }
+
     public void Add(Match match){
 
     }
 
     public void startMatch(String homeTeamName, String awayTeamName) {
+        matches.put(calculateMatchKey(homeTeamName, awayTeamName), new Match(homeTeamName, awayTeamName));
+    }
+
+    public boolean matchInProgress(String homeTeamName, String awayTeamName) {
+        return matches.containsKey(calculateMatchKey(homeTeamName, awayTeamName));
     }
 }
--- src/test/java/com/sportradar/codingexercise/ScoreboardTest.java
+++ src/test/java/com/sportradar/codingexercise/ScoreboardTest.java
@@ -2,6 +2,8 @@ package com.sportradar.codingexercise;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ScoreboardTest {
     @Test
     public void canAddMatchToScoreboard() {
@@ -16,5 +18,37 @@ public class ScoreboardTest {
         String homeTeamName = "HomeTeam";
         String awayTeamName = "AwayTeam";
         scoreboard.startMatch(homeTeamName, awayTeamName);
+    }
+
+    @Test
+    public void whenGivenTeamNamesThatArentInScoreBoard_thenMatchInProgressShallReturnFalse() {
+        Scoreboard scoreboard = new Scoreboard();
+        String homeTeamName = "HomeTeam";
+        String awayTeamName = "AwayTeam";
+        assertFalse(scoreboard.matchInProgress(homeTeamName, awayTeamName));
+    }
+    @Test
+    public void whenGivenTeamNamesThatArentInSameMatch_thenMatchInProgressShallReturnFalse() {
+        Scoreboard scoreboard = new Scoreboard();
+        String homeTeamName = "HomeTeam";
+        String awayTeamName = "AwayTeam";
+
+        String homeTeamNameSecondMatch = "HomeTeamSecondMatch";
+        String awayTeamNameSecondMatch = "AwayTeamSecondMatch";
+
+        scoreboard.startMatch(homeTeamName, awayTeamName);
+        scoreboard.startMatch(homeTeamNameSecondMatch, awayTeamNameSecondMatch);
+
+        assertFalse(scoreboard.matchInProgress(homeTeamNameSecondMatch, awayTeamName));
+        assertFalse(scoreboard.matchInProgress(homeTeamName, awayTeamNameSecondMatch));
+    }
+
+    @Test
+    public void whenGivenTeamNamesThatAreInSameMatch_thenMatchInProgressShallReturnTrue() {
+        Scoreboard scoreboard = new Scoreboard();
+        String homeTeamName = "HomeTeam";
+        String awayTeamName = "AwayTeam";
+        scoreboard.startMatch(homeTeamName, awayTeamName);
+        assertTrue(scoreboard.matchInProgress(homeTeamName, awayTeamName));
     }
 }
